9e52b034be61d2ae93235fdd3e04b711b7882c49
pedrohenrique57 <pedro.henrique.contato369@gmail.com>
2024-04-02 15:22:41 -0300

    Criação de uma Fila com generics
    Signed-off-by: pedrohenrique57 <pedro.henrique.contato369@gmail.com>

--- App/src/main/java/edu/DataAndAPIStream/ListasEPilhas/MainLisPil.java
+++ App/src/main/java/edu/DataAndAPIStream/ListasEPilhas/MainLisPil.java
@@ -5,17 +5,15 @@ import edu.DataAndAPIStream.EncadeamentoDeNo.No;
 public class MainLisPil {
 
     public static void main(String[] args) {
-
-        Pilha<Integer> minhaPilha = new Pilha<>();
-
         No<Integer> no1 = new No<>(1);
-
         No<Integer> no2 = new No<>(2);
         no2.setProximoNo(no1);
-
         No<Integer> no3 = new No<>(3);
         no2.setProximoNo(no3);
 
+
+
+        Pilha<Integer> minhaPilha = new Pilha<>();
         minhaPilha.push(no1);
         minhaPilha.push(no2);
         minhaPilha.push(no3);
@@ -25,5 +23,18 @@ public class MainLisPil {
         minhaPilha.pop();
 
         System.out.println(minhaPilha);
+
+
+
+        Fila<Integer> fila = new Fila<>();
+        fila.enqueue(no1);
+        fila.enqueue(no2);
+        fila.enqueue(no3);
+
+        System.out.println(fila);
+
+        System.out.println(fila.dequeue());
+
+        System.out.println(fila);
     }
 }
